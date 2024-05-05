89b5beb348b23b0ef7ba4f770c95c64301d4db12
Waleed Iqbal <wiqbal2@umbc.edu>
2024-05-05 00:16:04 -0400

    Some of the kernel implementation and almost all of the pieces movement are correclty working. Included the starting screen and while loop from the userspcae file

--- chess/chess.c
+++ chess/chess.c
@@ -6,8 +6,15 @@
 #include <linux/device.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
-
+#include <linux/string.h>  
+#include <linux/random.h>
+#define DEVICE_NAME "chessgame"
+#define CLASS_NAME "chess"
 #define BOARD_SIZE 8
+static int majorNumber;
+static struct class* chessClass = NULL;
+static struct device* chessDevice = NULL;
+static struct cdev c_dev;
 
 typedef enum {
     STATE_UNINITIALIZED,
@@ -20,6 +27,33 @@ typedef struct {
     char current_player;
     GameState state;
 } ChessGame;
+
+
+
+typedef struct {
+    int from_x, from_y; 
+    int to_x, to_y;     
+} Move;
+
+// store moves for cpu to choose
+typedef struct {
+    Move moves[1000]; // to hold moves
+    int count;        // num of moves 
+} MoveList;
+
+static ChessGame game;
+
+static int dev_open(struct inode *, struct file *);
+static int dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char __user *, size_t, loff_t *);
+
+static struct file_operations fops = {
+    .open = dev_open,
+    .release = dev_release,
+    .read = dev_read,
+    .write = dev_write,
+};
 
 // board size 8x8 and 3 for the W or B and piece as well as null term
 void initialize_board(char board[BOARD_SIZE][BOARD_SIZE][3]) {
@@ -251,7 +285,8 @@ int move_bishop(char board[BOARD_SIZE][BOARD_SIZE][3], char color, int from_x, i
         int tot_steps = diagx;
 
         // check all positions except for the last pos, need different case for the last pos
-        for (int i = 1; i < tot_steps; i++) {
+        int i = 0;
+        for ( i = 1; i < tot_steps; i++) {
             if (board[curr_y][curr_x][0] != '*') {
                 printk(KERN_INFO "Path is blocked when moving the bishop\n");
                 return -EINVAL;
@@ -353,8 +388,6 @@ int move_rook(char board[BOARD_SIZE][BOARD_SIZE][3], char color, int from_x, int
     }
 }
 
-
-
 int move_queen(char board[BOARD_SIZE][BOARD_SIZE][3], char color, int from_x, int from_y, int to_x, int to_y, int capture_bool) {
     // get move distance in x and y direction
     int distance_x = abs(to_x - from_x);
@@ -452,6 +485,14 @@ int move_king(char board[BOARD_SIZE][BOARD_SIZE][3], char color, int from_x, int
     }
 }
 
+static inline char upperCase(char c){
+	// convert to uppercase 
+	if( c >= 'a' && c <= 'z'){
+		return c - 'a' + 'A';
+	}
+	return c;
+}
+
 int handle_command(char board[BOARD_SIZE][BOARD_SIZE][3], const char *command) {
     char color, piece, from_col, to_col, promotion = '\0', promotion_piece = '\0';
     char captured_piece_type = '\0', capture = '\0';
@@ -460,9 +501,11 @@ int handle_command(char board[BOARD_SIZE][BOARD_SIZE][3], const char *command) {
     // buffer to hold command for manipulation
     char buf[32];
     strncpy(buf, command, sizeof(buf) - 1);
-    buf[sizeof(buf) - 1] = '\0'; // ensure null termination
+    // make null term
+    buf[sizeof(buf) - 1] = '\0'; 
 
     int offset = 0;
+    // go through the command 
     color = buf[offset++];
     piece = buf[offset++];
     from_col = buf[offset++];
@@ -503,9 +546,9 @@ int handle_command(char board[BOARD_SIZE][BOARD_SIZE][3], const char *command) {
         printk(KERN_INFO "Promotion indicated to %c\n", promotion_piece);
     }
 
-    char upper_piece = toupper(piece); // convert piece character to upper case
+    char upper_piece = upperCase(piece); // convert piece character to upper case
 
-    // Check which piece type and perform corresponding move
+    // check which piece type and perform corresponding move
     if (upper_piece == 'P') {
         return move_pawn(board, color, from_x, from_y, to_x, to_y, capture_bool);
     } else if (upper_piece == 'N') {
@@ -520,10 +563,169 @@ int handle_command(char board[BOARD_SIZE][BOARD_SIZE][3], const char *command) {
         return move_king(board, color, from_x, from_y, to_x, to_y, capture_bool);
     } else {
         printk(KERN_INFO "Invalid piece type.\n");
-        return -EINVAL; // return an invalid argument error
+        return -EINVAL; 
     }
 
-    return -EINVAL; // if no valid case was executed
+    return -EINVAL; 
+}
+
+// checks the path made for cpu 
+int is_path_clear(char board[BOARD_SIZE][BOARD_SIZE][3], int from_x, int from_y, int to_x, int to_y) {
+    if (from_x == to_x && from_y == to_y) {
+        return 1; // if the start and end are the same, no movement, thus path is 'clear'
+    }
+
+    int x_step = 0;
+    int y_step = 0;
+
+    if (to_x > from_x) {
+        x_step = 1;
+    } else if (to_x < from_x) {
+        x_step = -1;
+    } else {
+        x_step = 0;
+    }
+
+    if (to_y > from_y) {
+        y_step = 1;
+    } else if (to_y < from_y) {
+        y_step = -1;
+    } else {
+        y_step = 0;
+    }
+
+    int x = from_x + x_step;
+    int y = from_y + y_step;
+
+    while (x != to_x || y != to_y) {
+        if (board[y][x][0] != '*') {  // assuming '*' indicates an empty square
+            printk(KERN_INFO "path blocked at (%d, %d)\n", x, y);
+            return 0; // path is blocked
+        }
+        x += x_step;
+        y += y_step;
+    }
+
+    return 1; // path is clear
+}
+
+// helper for cpu
+int is_valid_move(char board[BOARD_SIZE][BOARD_SIZE][3], char color, int from_x, int from_y, int to_x, int to_y) {
+    // check if the piece at the starting position belongs to the player
+    if (board[from_y][from_x][0] != color) {
+        printk(KERN_INFO "invalid move: not player's piece.\n");
+        return 0;
+    }
+	printk("Entering is valid move");
+    char piece = board[from_y][from_x][1]; 
+    // get distance
+    int dx = to_x - from_x; 
+    int dy = to_y - from_y; 
+    
+    int abs_dx = abs(dx); 
+    int abs_dy = abs(dy); 
+
+    // check different types of pieces and their valid moves
+    if (piece == 'P') {
+        int forward;
+	if (color == 'W') {
+		forward = 1;
+	    } else {
+		forward = -1;
+	    }
+        if (dx == 0) {
+            if (dy == forward && board[to_y][to_x][0] == '*') {
+                return 1; // pawn can move one square forward if destination is empty
+            } // Check if the pawn can move two squares forward from the starting position if both squares are empty
+else if (dy == 2 * forward && (from_y == 1 || from_y == 6)) {
+    if (board[to_y][to_x][0] == '*' && board[from_y + forward][from_x][0] == '*') {
+        return 1;
+    }
+}
+        } else if (abs_dx == 1 && dy == forward && board[to_y][to_x][0] != '*' && board[to_y][to_x][0] != color) {
+            return 1; // pawn can capture diagonally
+        }
+    } else if (piece == 'N' && ((abs_dx == 2 && abs_dy == 1) || (abs_dx == 1 && abs_dy == 2))) {
+        return 1; // knight can move in an L-shape
+    } else if (piece == 'B' && abs_dx == abs_dy) {
+        return is_path_clear(board, from_x, from_y, to_x, to_y); // bishop can move diagonally
+    } else if (piece == 'R' && (dx == 0 || dy == 0)) {
+        return is_path_clear(board, from_x, from_y, to_x, to_y); // rook can move horizontally or vertically
+    } else if (piece == 'Q' && (abs_dx == abs_dy || dx == 0 || dy == 0)) {
+        return is_path_clear(board, from_x, from_y, to_x, to_y); // queen can move diagonally, horizontally, or vertically
+    } else if (piece == 'K' && abs_dx <= 1 && abs_dy <= 1) {
+        return 1; // king can move one square in any direction
+    } else {
+        printk(KERN_INFO "invalid move attempted by %c from (%d, %d) to (%d, %d)\n", piece, from_x, from_y, to_x, to_y);
+        return 0; // invalid move for any other cases
+    }
+
+    return 0; // default return
+}
+
+//helper for cpu
+void collect_valid_moves(char board[BOARD_SIZE][BOARD_SIZE][3], char color, MoveList *move_list) {
+    // reset the list of moves
+    move_list->count = 0;
+    int y,dy,dx,x = 0;
+	printk("Entering collect valid move");
+    // iterate over the board to find the player's pieces
+    for ( y = 0; y < BOARD_SIZE; y++) {
+        for ( x = 0; x < BOARD_SIZE; x++) {
+
+            if (board[y][x][0] == color) {
+              
+                for ( dy = 0; dy < BOARD_SIZE; dy++) {
+                    for ( dx = 0; dx < BOARD_SIZE; dx++) {
+                        // check if the move drom dx dy is valid
+                        if (is_valid_move(board, color, x, y, dx, dy)) {
+                            // if the move is valid, add it to the move list
+                            if (move_list->count < 1000) {
+                                move_list->moves[move_list->count].from_x = x;
+                                move_list->moves[move_list->count].from_y = y;
+                                move_list->moves[move_list->count].to_x = dx;
+                                move_list->moves[move_list->count].to_y = dy;
+                                move_list->count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+void execute_cpu_move(char board[BOARD_SIZE][BOARD_SIZE][3], char color) {
+    MoveList move_list; 
+
+    
+    collect_valid_moves(board, color, &move_list);
+	printk("Entering is execute move");
+    // Check if there are any valid moves available
+    if (move_list.count > 0) {
+        unsigned int index;
+        // generate a random index within the range of valid moves
+        get_random_bytes(&index, sizeof(index));
+        index %= move_list.count; // checkthe index is within the range of valid moves
+
+    
+        Move selected_move = move_list.moves[index];
+        // update move
+        board[selected_move.to_y][selected_move.to_x][0] = board[selected_move.from_y][selected_move.from_x][0];
+        board[selected_move.to_y][selected_move.to_x][1] = board[selected_move.from_y][selected_move.from_x][1];
+        board[selected_move.from_y][selected_move.from_x][0] = '*'; // empty pos
+        board[selected_move.from_y][selected_move.from_x][1] = '*';
+
+        // print the move
+        printk(KERN_INFO "CPU moved from (%d, %d) to (%d, %d)\n",
+               selected_move.from_x, selected_move.from_y,
+               selected_move.to_x, selected_move.to_y);
+    } else {
+        // no valid moves are available, print a message to the kernel log
+        printk(KERN_INFO "No valid moves available for CPU.\n");
+    }
 }
 int process_command(ChessGame *game, const char *command) {
 
@@ -535,7 +737,7 @@ int process_command(ChessGame *game, const char *command) {
         }
         printk(KERN_INFO "malformed command\n");
         return -EINVAL; // malformed command
-    } else if (strcmp(command, "01\n") == 0) {
+    } else if (strncmp(command, "01\n",3) == 0) {
         // check if there's a game running to display its state
         if (game->state != STATE_RUNNING) {
             printk(KERN_INFO "NOGAME\n");
@@ -557,9 +759,22 @@ int process_command(ChessGame *game, const char *command) {
         }
         return 0;
     } else if (strcmp(command, "03\n") == 0) {
-        // CPU move logic here
-        return -EINVAL;
-    } else if (strcmp(command, "04\n") == 0) {
+    printk(KERN_INFO"Entering number 3");
+        if (game->state != STATE_RUNNING) {
+            printk(KERN_INFO "NOGAME\n");
+            return -EINVAL;
+        }
+        else{
+	if(game->current_player == 'W'){
+		execute_cpu_move(game->board,'B');
+	}
+	else{
+		execute_cpu_move(game->board,'W');
+	}
+        }
+        return 0;
+    } 
+    else if (strncmp(command, "04\n",3) == 0) {
         // handle game resignation
         if (game->state != STATE_RUNNING) {
             printk(KERN_INFO "NOGAME\n");
@@ -569,11 +784,96 @@ int process_command(ChessGame *game, const char *command) {
         printk(KERN_INFO "Game resigned. CPU wins.\n");
         return 0;
     }
-    printk(KERN_INFO "unknown or malformed command\n");
-    return -EINVAL; // unknown or malformed command
+    printk(KERN_INFO "unknown command\n");
+    return -EINVAL; 
+}
+
+static int __init chess_init(void) {
+    printk(KERN_INFO "Chess: Initializing the Chess \n");
+    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+    if (majorNumber < 0) {
+        printk(KERN_ALERT "Chess failed to register a major number\n");
+        return majorNumber;
+    }
+
+    chessClass = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(chessClass)) {
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+        return PTR_ERR(chessClass);
+    }
+
+    chessDevice = device_create(chessClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(chessDevice)) {
+        class_destroy(chessClass);
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+        return PTR_ERR(chessDevice);
+    }
+
+    cdev_init(&c_dev, &fops);
+    cdev_add(&c_dev, MKDEV(majorNumber, 0), 1);
+    printk(KERN_INFO "Chess Device class created correctly\n");
+    return 0;
+}
+
+static void __exit chess_exit(void) {
+    cdev_del(&c_dev);
+    device_destroy(chessClass, MKDEV(majorNumber, 0));
+    class_destroy(chessClass);
+    unregister_chrdev(majorNumber, DEVICE_NAME);
+    printk(KERN_INFO "Chess end!\n");
+}
+
+static int dev_open(struct inode *inodep, struct file *filep) {
+    printk(KERN_INFO "Chess Device has been opened\n");
+    return 0;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep) {
+    printk(KERN_INFO "Chess Device successfully closed\n");
+    return 0;
 }
 
 
-	
+static ssize_t dev_read(struct file *filep, char __user *buffer, size_t len, loff_t *offset) {
+    static const char *msg = "Current game state\n";
+    int msg_size = strlen(msg);
 
+    if (*offset >= msg_size) return 0; // EOF
+
+    if (len > msg_size - *offset) len = msg_size - *offset;
+
+    if (copy_to_user(buffer, msg + *offset, len)) return -EFAULT;
+
+    *offset += len;
+    return len; // Return the number of bytes read
+}
+
+static ssize_t dev_write(struct file *filep, const char __user *buffer, size_t len, loff_t *offset) {
+	// allocate mem and intiialize it to 0
+    char *kbuf = kzalloc(len+1, GFP_KERNEL);
+    if (!kbuf)
+        return -ENOMEM;
+
+    if (copy_from_user(kbuf, buffer, len)) {
+        kfree(kbuf);
+        return -EFAULT;
+    }
+// null the string
+    kbuf[len] = '\0'; 
+    printk(KERN_INFO "Chess: Received command - %s\n", kbuf);
+   
+    int ret = process_command(&game, kbuf);
+    kfree(kbuf);
+    if (ret != 0) {
+        return ret;  //the error or specific non-zero return value
+    } else {
+        return len;  //the number of bytes processed if no errors
+    }
+}
+module_init(chess_init);
+module_exit(chess_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Waleed");
+MODULE_DESCRIPTION("Chess Game");
 
